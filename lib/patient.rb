97717f7dbb9c113294fba81ee84518767bc26b9a
Jack Madden <jck13mad@gmail.com>
2020-04-04 21:34:52 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,7 +11,7 @@ class Patient
   
   def new_appointment(doctor, date)
     Appointment.new(date, self, appointment)
-    @@appointments << appointments
+    @@appointments << appointment
   end
   
   def appointments
