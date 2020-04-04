2149a9cbde521abbfd5ae922dd8b6bf916acafcc
Jack Madden <jck13mad@gmail.com>
2020-04-04 22:46:42 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,8 +1,11 @@
 class Patient
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name=name
+    @@all << self
   end
   
    def new_appointment(doctor, date)
@@ -19,6 +22,10 @@ class Patient
     appointments.map do |appointment|
       appointment.doctor
     end
+  end
+  
+  def self.all
+    @@all
   end
   
 end
