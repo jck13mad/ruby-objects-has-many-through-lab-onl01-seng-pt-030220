4bb475a00329565ecf9fde5fd853d21d67568332
Jack Madden <jck13mad@gmail.com>
2020-04-04 22:43:57 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,27 +1,27 @@
 class Doctor
-
-  attr_accessor :name
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def appointments
-    Appointment.all.select {|appointment| appointment.doctor == self}
-  end
-
-  def patients
-    appointments.map {|appointment| appointment.patient}
-  end
+  @@all=[]
 
   def self.all
     @@all
   end
 
-  def new_appointment(patient, date)
-    Appointment.new(patient, date, self)
+  attr_accessor :name, :appointments, :patient
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @@all << self
   end
 
+  def new_appointment(date, patient)
+     appointment = Appointment.new(patient, date, self)
+     @appointments << appointment
+     appointment
+  end
+
+  def patients
+    @appointments.collect do |appointment|
+      appointment.patient
+    end
+  end
 end
